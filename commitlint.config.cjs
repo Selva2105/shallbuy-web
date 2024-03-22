d8f49dbed2157bc5acd998324f2963090e7b139c
Selvaganapathi_Kanakaraj <selvaganapathikanakaraj2105@gmail.com>
2024-03-22 10:06:47 +0530

    ci: workflows init

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -7,7 +7,7 @@
 // refactor: A code change that neither fixes a bug nor adds a feature
 // style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
 // test: Adding missing tests or correcting existing tests
-
+// bck : Creating a backup for the reference
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
@@ -44,6 +44,7 @@ module.exports = {
         "translation",
         "security",
         "changeset",
+        "bck"
       ],
     ],
   },
